a2103d7b9312f5e6ee07d0b91b7e948718f15cd9
Mark Dessain <mark.dessain@simplybusiness.co.uk>
2018-07-17 14:18:22 +0100

    ssm parameters

---
+++ node/parameters.sh
@@ -0,0 +1,5 @@
+aws --profile serverless_personal --region eu-west-1 ssm put-parameter --name /workshop-node/dev/table_name --type String --value restaurants
+aws --profile serverless_personal --region eu-west-1 ssm put-parameter --name /workshop-node/dev/cognito_user_pool_id --type String --value eu-west-1_z9AyExIfw
+aws --profile serverless_personal --region eu-west-1 ssm put-parameter --name /workshop-node/dev/cognito_web_client_id --type String --value 29oc5lf4tonbodmuhom07b1srm
+aws --profile serverless_personal --region eu-west-1 ssm put-parameter --name /workshop-node/dev/cognito_server_client_id --type String --value 687aijjbjc8nu69qjjr7oglu96
+aws --profile serverless_personal --region eu-west-1 ssm put-parameter --name /workshop-node/dev/url --type String --value 4qkmw6oovb.execute-api.eu-west-1.amazonaws.com/dev
--- node/tests/steps/init.js
+++ node/tests/steps/init.js
@@ -1,27 +1,56 @@
+const _ = require('lodash')
 const { promisify } = require('util')
 const awscred = require('awscred')
+const { REGION, STAGE } = process.env
 const AWS = require('aws-sdk')
+AWS.config.region = REGION
+const SSM = new AWS.SSM()
 
 let initialized = false
 
+const getParameters = async (keys) => {
+  const prefix = `/workshop-node/${STAGE}/`
+  const req = {
+    Names: keys.map(key => `${prefix}${key}`)
+  }
+  const resp = await SSM.getParameters(req).promise()
+  return _.reduce(resp.Parameters, function(obj, param) {
+    obj[param.Name.substr(prefix.length)] = param.Value
+    return obj
+   }, {})
+}
 
 const init = async () => {
   if (initialized) {
     return
   }
 
-  process.env.TEST_ROOT = "https://4qkmw6oovb.execute-api.eu-west-1.amazonaws.com/dev"
-  process.env.restaurants_api      = "https://4qkmw6oovb.execute-api.eu-west-1.amazonaws.com/dev/restaurants"
-  process.env.restaurants_table    = "restaurants"
-  process.env.AWS_REGION           = "eu-west-1"
-  process.env.cognito_user_pool_id = "eu-west-1_z9AyExIfw"
-  process.env.cognito_client_id    = "29oc5lf4tonbodmuhom07b1srm"
-  process.env.cognito_server_client_id = "687aijjbjc8nu69qjjr7oglu96"
+  const params = await getParameters([
+    'table_name',
+    'cognito_user_pool_id',
+    'cognito_web_client_id',
+    'cognito_server_client_id',
+    'url'
+  ])
 
-  const { credentials } = await promisify(awscred.load)({profile: 'serverless_personal'})
+  console.log('SSM params loaded')
+
+  process.env.TEST_ROOT                = `https://${params.url}`
+  process.env.restaurants_api          = `https://${params.url}/restaurants`
+  process.env.restaurants_table        = params.table_name
+  process.env.AWS_REGION               = REGION
+  process.env.cognito_user_pool_id     = params.cognito_user_pool_id
+  process.env.cognito_client_id        = params.cognito_web_client_id
+  process.env.cognito_server_client_id = params.cognito_server_client_id
+
+  const { credentials } = await promisify(awscred.load)()
 
   process.env.AWS_ACCESS_KEY_ID     = credentials.accessKeyId
   process.env.AWS_SECRET_ACCESS_KEY = credentials.secretAccessKey
+
+  if (credentials.sessionToken) {
+    process.env.AWS_SESSION_TOKEN = credentials.sessionToken
+  }
 
   console.log('AWS credential loaded')
 
